435273159bb34739772907665fe7aac2d879dbfa
huangqing <2316694914@qq.com>
2021-02-04 17:51:21 +0800

    新增 sass-resources-loader已经添加全局less变量

--- angular.webpack.js
+++ angular.webpack.js
@@ -19,5 +19,20 @@ module.exports = (config, options) => {
             break;
         }
     }
+    config.module.rules.push({
+        test: /\.less$/,
+        use:{
+            loader: 'sass-resources-loader',
+            options: {
+                resources: [
+                    './src/less/base/variable.less',
+                    './src/less/util.less',
+                    './src/less/base/box.less',
+                    './src/less/base/mixin/setArrow.less',
+                    './src/less/base/mixin/setOnepx.less'
+                ]
+            }
+        }}
+    )
     return config;
 }
