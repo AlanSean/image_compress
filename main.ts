04404e5bf073e806d7aac78afd76620d85e82f1b
huangqing <2316694914@qq.com>
2021-02-24 16:04:06 +0800

    修改很多配置 不一一记录了

--- main.ts
+++ main.ts
@@ -1,54 +1,58 @@
-import { app, BrowserWindow, screen } from 'electron';
-import * as path from 'path';
-import * as url from 'url';
+import * as os from "os";
+import * as path from "path";
+import * as url from "url";
+import { app, BrowserWindow, ipcMain } from "electron";
+import { loadExtension, setProtocol } from './electron';
 
 let win: BrowserWindow = null;
 const args = process.argv.slice(1),
-  serve = args.some(val => val === '--serve');
+  serve = args.some((val) => val === "--serve");
 
 function createWindow(): BrowserWindow {
-
-  const electronScreen = screen;
-  const size = electronScreen.getPrimaryDisplay().workAreaSize;
-
   // Create the browser window.
   win = new BrowserWindow({
-    x: 0,
-    y: 0,
-    width: size.width,
-    height: size.height,
+    width: 800 + (os.platform() === "darwin" ? 15 : 34),
+    height: 600,
+    minWidth: 540,
     webPreferences: {
+      webSecurity: false, //允许加载本地资源
       nodeIntegration: true,
-      allowRunningInsecureContent: (serve) ? true : false,
-      contextIsolation: false,  // false if you want to run 2e2 test with Spectron
-      enableRemoteModule : true // true if you want to run 2e2 test  with Spectron or use remote module in renderer context (ie. Angular)
+      // allowRunningInsecureContent: serve ? true : false,
+      contextIsolation: false, // false if you want to run 2e2 test with Spectron
+      enableRemoteModule: true, // true if you want to run 2e2 test  with Spectron or use remote module in renderer context (ie. Angular)
     },
   });
 
   if (serve) {
-
     win.webContents.openDevTools();
 
-    require('electron-reload')(__dirname, {
-      electron: require(`${__dirname}/node_modules/electron`)
+    require("electron-reload")(__dirname, {
+      electron: require(`${__dirname}/node_modules/electron`),
     });
-    win.loadURL('http://localhost:4200');
 
+    //安装扩展
+    loadExtension();
+
+    win.loadURL("http://localhost:4200");
   } else {
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'dist/index.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
+    win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "dist/index.html"),
+        protocol: "file:",
+        slashes: true,
+      })
+    );
   }
 
   // Emitted when the window is closed.
-  win.on('closed', () => {
+  win.on("closed", () => {
     // Dereference the window object, usually you would store window
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     win = null;
   });
+  //设置自定义协议
+  setProtocol();
 
   return win;
 }
@@ -58,25 +62,29 @@ try {
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
   // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
-  app.on('ready', () => setTimeout(createWindow, 400));
+  app.on("ready", function () {
+    const win = createWindow();
+    ipcMain.on("onDrop", (event, files) => {
+      win.webContents.send("file_selected", files);
+    });
+  });
 
   // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
+  app.on("window-all-closed", () => {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
+    if (process.platform !== "darwin") {
       app.quit();
     }
   });
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (win === null) {
       createWindow();
     }
   });
-
 } catch (e) {
   // Catch Error
   // throw e;
