8be8a07c5e95aa7db4d5a1f857f8a972e769d7f0
huangqing <2316694914@qq.com>
2021-03-04 18:15:08 +0800

    添加主进程事件  文件添加

--- electronConfig/listenIpc.ts
+++ electronConfig/listenIpc.ts
@@ -1,10 +1,12 @@
 import { ipcMain, BrowserWindow } from "electron";
 import { IpcChannel } from "../src/common/constants";
-
+export interface fileObj {
+  filepath: string;
+  outpath: string;
+}
 
 export function listenIpc(win: BrowserWindow): void {
-  ipcMain.on(IpcChannel.FILE_ADD, (_, files:string[]) => {
-    // receiveFiles(win,files);
+  ipcMain.on(IpcChannel.FILE_ADD, (_, files: string[]) => {
     win.webContents.send(IpcChannel.FILE_SELECTED, files);
   });
 }
