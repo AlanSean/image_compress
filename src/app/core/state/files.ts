5e50edd1285091590481b697691ee7386d2d2804
huangqing <2316694914@qq.com>
2021-03-04 18:14:45 +0800

    试图改为hashmap操作ngrx

--- src/app/core/state/files.ts
+++ src/app/core/state/files.ts
@@ -5,49 +5,89 @@ import {
   on,
   props,
 } from "@ngrx/store";
+import { HashMap }  from '@utils/index';
 
+const fileMap = new HashMap<file>();
 // state
-export interface FilesState {
-  files: ReadonlyArray<string>;
+export interface file{
+  path: string;
+  outpath: string;
+  state: string;
 }
+export interface fileArr {
+  [key:string]: file
+}
+export interface FilesState {
+  fileArr: fileArr;
+}
+
 //action
 export const FILE_ADD = createAction(
-  "[Book List] Add Book",
-  props<{ files: string[] }>()
+  "添加图片",
+  props<{ files: file | Array<file> }>()
 );
 export const removeFILE = createAction(
-  "[Book Collection] Remove Book",
+  "删除图片 & 批量删除",
   props<{ filePath: string | Array<string> }>()
 );
-//获取图片列表
-export const retrievedFILEList = createAction(
-  "[Book List/API] Retrieve Books Success",
-  props<{ Book }>()
+
+export const UPDATE_STATE = createAction(
+  "更新图片压缩状态",
+  props<{ file: file }>()
 );
 
 //reducer
-const initialState: ReadonlyArray<string> = [];
-export const booksReducer = createReducer(
+const initialState: fileArr = fileMap.val;
+export const fileReducer = createReducer(
   initialState,
   on(removeFILE, (state, { filePath }) => {
-    if (Array.isArray(filePath)) {
-      return state.filter((path) => filePath.includes(path));
-    } else {
-      return state.filter((path) => path !== filePath);
-    }
+
+    fileMap.delete(filePath);
+    return {
+      ...fileMap.val
+    };
   }),
   on(FILE_ADD, (state, { files }) => {
-    const newFilePath: Array<string> = [];
-    files.forEach((path: string) => {
-      path = `file://${path}`;
-      !state.includes(path) && newFilePath.push(path);
+    //如果是数组
+    if (Array.isArray(files)) {
+      for (const file of files) {
+        //非png jpeg格式直接跳过
+        if (!/png|jpeg|jpg/.test(file.path)) continue;
+
+        fileMap.put(file.path,{
+          ...file,
+          path: `file://${file.path}`
+        });
+      }
+      
+    } else if (/png|jpeg|jpg/.test(files.path)) {
+      fileMap.put(files.path,{
+        ...files,
+        path: `file://${files.path}`
+      });
+    }
+    return {
+      ...fileMap.val
+    };
+  }),
+  on(UPDATE_STATE, (state, { file }) => {
+    fileMap.put(file.path,{
+      ...file,
+      path: `file://${file.path}`
     });
-    return [...state, ...newFilePath];
+    return {
+      ...fileMap.val
+    };
   })
 );
 
 //selectter
 export const selectFile = createSelector(
-  (state: FilesState) => state.files,
-  (files: Array<string>) => files
+  (state: FilesState) => {
+    console.log(state);
+    return state.fileArr;
+  },
+  (fileArr: fileArr) => {
+    return fileArr;
+  }
 );
