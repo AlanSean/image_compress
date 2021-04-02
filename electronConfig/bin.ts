a90533eed8c54e5dcfebe9a868c1a987fcda2f32
huangqing <2316694914@qq.com>
2021-04-02 17:07:10 +0800

    feat：接入mozjpeg 压缩jpeg功能

--- electronConfig/bin.ts
+++ electronConfig/bin.ts
@@ -4,47 +4,86 @@ import { resolve } from "path";
 import log from "electron-log";
 import Local_Bin_Wrapper from "./Local_Bin_Wrapper";
 
-const url = resolve(__dirname, "../bin");
-const pngquantBin = new Local_Bin_Wrapper()
-  .src(`${url}/mac/pngquant/pngquant`, "darwin")
-  .src(`${url}/win/pngquant/pngquant.exe`, "win32")
-  .path();
-const args = ["-", "--force", "--quality"];
+const url = resolve(__dirname, "../bin"),
+  pngquantBin = new Local_Bin_Wrapper()
+    .src(`${url}/mac/pngquant`, "darwin")
+    .src(`${url}/win/pngquant.exe`, "win32")
+    .path(),
+  pngquantArgs = ["-", "--force", "--quality"],
+  mozJpegBin = new Local_Bin_Wrapper()
+    .src(`${url}/mac/cjpeg`, "darwin")
+    .src(`${url}/win/cjpeg.exe`, "win32")
+    .path(),
+  mozJpegargs = ["--quality"];
 
-
-interface imageInfo{
-  status: number,
-  rawDataSize: number,
-  data: Buffer |string,
-  nowDataSize: number,
+interface imageInfo {
+  status: number;
+  rawDataSize: number;
+  data: Buffer | string;
+  nowDataSize: number;
 }
-
+//version 2.12.0
 export const pngquant = (options: { quality: string }) => async (
   path: string
 ): Promise<imageInfo> => {
-  args[3] = options.quality;
+  pngquantArgs[3] = options.quality;
 
   const input = await fs.readFile(path);
 
-  const childProcess = execa(pngquantBin, args, {
+  const childProcess = execa(pngquantBin, pngquantArgs, {
     encoding: null,
     maxBuffer: Infinity,
     input,
   })
-    .then( result => {
+    .then((result) => {
       return {
         status: 0,
-        rawDataSize:input.length,
+        rawDataSize: input.length,
         data: result.stdout,
         nowDataSize: result.stdout.length,
       };
     })
-    .catch( error => { 
+    .catch((error) => {
       //https://github.com/kornelski/pngquant#--quality-min-max
       if (error.exitCode === 99) {
-        return{
+        return {
           status: 99,
-          rawDataSize:input.length,
+          rawDataSize: input.length,
+          data: input,
+          nowDataSize: 0,
+        };
+      }
+      log.error("pngquanterror:", error);
+    });
+  return childProcess;
+};
+
+//version 2.12.0
+export const mozjpeg = (options: { quality: string }) => async (
+  path: string
+): Promise<imageInfo> => {
+  mozJpegargs[1] = options.quality;
+
+  const input = await fs.readFile(path);
+
+  const childProcess = execa(mozJpegBin, mozJpegargs, {
+    encoding: null,
+    maxBuffer: Infinity,
+    input,
+  })
+    .then((result) => {
+      return {
+        status: 0,
+        rawDataSize: input.length,
+        data: result.stdout,
+        nowDataSize: result.stdout.length,
+      };
+    })
+    .catch((error) => {
+      if (error.exitCode === 99) {
+        return {
+          status: 99,
+          rawDataSize: input.length,
           data: input,
           nowDataSize: 0,
         };
