ba2c2f75a3c7ba6e529d0bc283dc72ffa75af2ac
huangqing <2316694914@qq.com>
2021-04-20 18:01:20 +0800

    chore:  新增 删除逻辑 优化 重新选择质量时压缩逻辑

--- src/app/shared/components/img-list/img-list.component.ts
+++ src/app/shared/components/img-list/img-list.component.ts
@@ -1,234 +1,269 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit,  ChangeDetectionStrategy,
+  ChangeDetectorRef, } from "@angular/core";
 import { Store, select } from "@ngrx/store";
 import { Observable } from "rxjs";
 import { FILE } from "@common/constants";
-import { selectFile } from "@app/core/core.module";
-import { UPDATE_STATE } from "@app/core/core.module";
+import { selectFile, UPDATE_STATE,REMOVE_FILE } from "@app/core/core.module";
+import { ElectronService } from "@app/core/services";
 
 @Component({
   selector: "app-img-list",
   templateUrl: "./img-list.component.html",
   styleUrls: ["./img-list.component.less"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ImgListComponent implements OnInit {
   files$: Observable<ReadonlyArray<FILE>>;
-  files = [
-    {
-      state: "finish",
-      percentage: "-9.8%",
-      src: "file://C:/Users/111/Desktop/copy/start.jpg",
-      path: "C:/Users/111/Desktop/copy/start.jpg",
-      extname: ".jpg",
-      ext: "jpg",
-      outsrc:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
-      outpath:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
-      quality: "80",
-      rawDataSize: "5.57 KB",
-      nowDataSize: "2.28 KB",
-    },
-    {
-      state: "finish",
-      percentage: "-9.8%",
-      src: "file://C:/Users/111/Desktop/copy/start.jpg",
-      path: "C:/Users/111/Desktop/copy/start.jpg",
-      extname: ".jpg",
-      ext: "jpg",
-      outsrc:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
-      outpath:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
-      quality: "80",
-      rawDataSize: "5.57 KB",
-      nowDataSize: "2.28 KB",
-    },
-    {
-      state: "finish",
-      percentage: "-9.8%",
-      src: "file://C:/Users/111/Desktop/copy/start.jpg",
-      path: "C:/Users/111/Desktop/copy/start.jpg",
-      extname: ".jpg",
-      ext: "jpg",
-      outsrc:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
-      outpath:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
-      quality: "80",
-      rawDataSize: "5.57 KB",
-      nowDataSize: "2.28 KB",
-    },
-    {
-      state: "finish",
-      percentage: "-9.8%",
-      src: "file://C:/Users/111/Desktop/copy/start.jpg",
-      path: "C:/Users/111/Desktop/copy/start.jpg",
-      extname: ".jpg",
-      ext: "jpg",
-      outsrc:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
-      outpath:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
-      quality: "80",
-      rawDataSize: "5.57 KB",
-      nowDataSize: "2.28 KB",
-    },
-    {
-      state: "finish",
-      percentage: "-9.8%",
-      src: "file://C:/Users/111/Desktop/copy/start.jpg",
-      path: "C:/Users/111/Desktop/copy/start.jpg",
-      extname: ".jpg",
-      ext: "jpg",
-      outsrc:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
-      outpath:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
-      quality: "80",
-      rawDataSize: "5.57 KB",
-      nowDataSize: "2.28 KB",
-    },
-    {
-      state: "finish",
-      percentage: "-9.8%",
-      src: "file://C:/Users/111/Desktop/copy/start.jpg",
-      path: "C:/Users/111/Desktop/copy/start.jpg",
-      extname: ".jpg",
-      ext: "jpg",
-      outsrc:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
-      outpath:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
-      quality: "80",
-      rawDataSize: "5.57 KB",
-      nowDataSize: "2.28 KB",
-    },
-    {
-      state: "finish",
-      percentage: "-9.8%",
-      src: "file://C:/Users/111/Desktop/copy/start.jpg",
-      path: "C:/Users/111/Desktop/copy/start.jpg",
-      extname: ".jpg",
-      ext: "jpg",
-      outsrc:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
-      outpath:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
-      quality: "80",
-      rawDataSize: "5.57 KB",
-      nowDataSize: "2.28 KB",
-    },
-    {
-      state: "finish",
-      percentage: "-9.8%",
-      src: "file://C:/Users/111/Desktop/copy/start.jpg",
-      path: "C:/Users/111/Desktop/copy/start.jpg",
-      extname: ".jpg",
-      ext: "jpg",
-      outsrc:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
-      outpath:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
-      quality: "80",
-      rawDataSize: "5.57 KB",
-      nowDataSize: "2.28 KB",
-    },
-    {
-      state: "finish",
-      percentage: "-9.8%",
-      src: "file://C:/Users/111/Desktop/copy/start.jpg",
-      path: "C:/Users/111/Desktop/copy/start.jpg",
-      extname: ".jpg",
-      ext: "jpg",
-      outsrc:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
-      outpath:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
-      quality: "80",
-      rawDataSize: "5.57 KB",
-      nowDataSize: "2.28 KB",
-    },
-    {
-      state: "finish",
-      percentage: "-9.8%",
-      src: "file://C:/Users/111/Desktop/copy/start.jpg",
-      path: "C:/Users/111/Desktop/copy/start.jpg",
-      extname: ".jpg",
-      ext: "jpg",
-      outsrc:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
-      outpath:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
-      quality: "80",
-      rawDataSize: "5.57 KB",
-      nowDataSize: "2.28 KB",
-    },
-    {
-      state: "finish",
-      percentage: "-9.8%",
-      src: "file://C:/Users/111/Desktop/copy/start.jpg",
-      path: "C:/Users/111/Desktop/copy/start.jpg",
-      extname: ".jpg",
-      ext: "jpg",
-      outsrc:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
-      outpath:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
-      quality: "80",
-      rawDataSize: "5.57 KB",
-      nowDataSize: "2.28 KB",
-    },
-    {
-      state: "finish",
-      percentage: "-9.8%",
-      src: "file://C:/Users/111/Desktop/copy/start.jpg",
-      path: "C:/Users/111/Desktop/copy/start.jpg",
-      extname: ".jpg",
-      ext: "jpg",
-      outsrc:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
-      outpath:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
-      quality: "80",
-      rawDataSize: "5.57 KB",
-      nowDataSize: "2.28 KB",
-    },
-    {
-      state: "finish",
-      percentage: "-9.8%",
-      src: "file://C:/Users/111/Desktop/copy/start.jpg",
-      path: "C:/Users/111/Desktop/copy/start.jpg",
-      extname: ".jpg",
-      ext: "jpg",
-      outsrc:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
-      outpath:
-        "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
-      quality: "80",
-      rawDataSize: "5.57 KB",
-      nowDataSize: "2.28 KB",
-    },
-  ];
-  constructor(private store: Store<ReadonlyArray<FILE>>) {
+  // files = [
+  //   {
+  //     state: "finish",
+  //     percentage: "-9.8%",
+  //     src: "file://C:/Users/111/Desktop/copy/start.jpg",
+  //     path: "C:/Users/111/Desktop/copy/start.jpg",
+  //     extname: ".jpg",
+  //     ext: "jpg",
+  //     outsrc:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
+  //     outpath:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
+  //     quality: "80",
+  //     rawDataSize: "5.57 KB",
+  //     nowDataSize: "2.28 KB",
+  //   },
+  //   {
+  //     state: "finish",
+  //     percentage: "-9.8%",
+  //     src: "file://C:/Users/111/Desktop/copy/start.jpg",
+  //     path: "C:/Users/111/Desktop/copy/start.jpg",
+  //     extname: ".jpg",
+  //     ext: "jpg",
+  //     outsrc:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
+  //     outpath:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
+  //     quality: "80",
+  //     rawDataSize: "5.57 KB",
+  //     nowDataSize: "2.28 KB",
+  //   },
+  //   {
+  //     state: "finish",
+  //     percentage: "-9.8%",
+  //     src: "file://C:/Users/111/Desktop/copy/start.jpg",
+  //     path: "C:/Users/111/Desktop/copy/start.jpg",
+  //     extname: ".jpg",
+  //     ext: "jpg",
+  //     outsrc:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
+  //     outpath:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
+  //     quality: "80",
+  //     rawDataSize: "5.57 KB",
+  //     nowDataSize: "2.28 KB",
+  //   },
+  //   {
+  //     state: "finish",
+  //     percentage: "-9.8%",
+  //     src: "file://C:/Users/111/Desktop/copy/start.jpg",
+  //     path: "C:/Users/111/Desktop/copy/start.jpg",
+  //     extname: ".jpg",
+  //     ext: "jpg",
+  //     outsrc:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
+  //     outpath:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
+  //     quality: "80",
+  //     rawDataSize: "5.57 KB",
+  //     nowDataSize: "2.28 KB",
+  //   },
+  //   {
+  //     state: "finish",
+  //     percentage: "-9.8%",
+  //     src: "file://C:/Users/111/Desktop/copy/start.jpg",
+  //     path: "C:/Users/111/Desktop/copy/start.jpg",
+  //     extname: ".jpg",
+  //     ext: "jpg",
+  //     outsrc:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
+  //     outpath:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
+  //     quality: "80",
+  //     rawDataSize: "5.57 KB",
+  //     nowDataSize: "2.28 KB",
+  //   },
+  //   {
+  //     state: "finish",
+  //     percentage: "-9.8%",
+  //     src: "file://C:/Users/111/Desktop/copy/start.jpg",
+  //     path: "C:/Users/111/Desktop/copy/start.jpg",
+  //     extname: ".jpg",
+  //     ext: "jpg",
+  //     outsrc:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
+  //     outpath:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
+  //     quality: "80",
+  //     rawDataSize: "5.57 KB",
+  //     nowDataSize: "2.28 KB",
+  //   },
+  //   {
+  //     state: "finish",
+  //     percentage: "-9.8%",
+  //     src: "file://C:/Users/111/Desktop/copy/start.jpg",
+  //     path: "C:/Users/111/Desktop/copy/start.jpg",
+  //     extname: ".jpg",
+  //     ext: "jpg",
+  //     outsrc:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
+  //     outpath:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
+  //     quality: "80",
+  //     rawDataSize: "5.57 KB",
+  //     nowDataSize: "2.28 KB",
+  //   },
+  //   {
+  //     state: "finish",
+  //     percentage: "-9.8%",
+  //     src: "file://C:/Users/111/Desktop/copy/start.jpg",
+  //     path: "C:/Users/111/Desktop/copy/start.jpg",
+  //     extname: ".jpg",
+  //     ext: "jpg",
+  //     outsrc:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
+  //     outpath:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
+  //     quality: "80",
+  //     rawDataSize: "5.57 KB",
+  //     nowDataSize: "2.28 KB",
+  //   },
+  //   {
+  //     state: "finish",
+  //     percentage: "-9.8%",
+  //     src: "file://C:/Users/111/Desktop/copy/start.jpg",
+  //     path: "C:/Users/111/Desktop/copy/start.jpg",
+  //     extname: ".jpg",
+  //     ext: "jpg",
+  //     outsrc:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
+  //     outpath:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
+  //     quality: "80",
+  //     rawDataSize: "5.57 KB",
+  //     nowDataSize: "2.28 KB",
+  //   },
+  //   {
+  //     state: "finish",
+  //     percentage: "-9.8%",
+  //     src: "file://C:/Users/111/Desktop/copy/start.jpg",
+  //     path: "C:/Users/111/Desktop/copy/start.jpg",
+  //     extname: ".jpg",
+  //     ext: "jpg",
+  //     outsrc:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
+  //     outpath:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
+  //     quality: "80",
+  //     rawDataSize: "5.57 KB",
+  //     nowDataSize: "2.28 KB",
+  //   },
+  //   {
+  //     state: "finish",
+  //     percentage: "-9.8%",
+  //     src: "file://C:/Users/111/Desktop/copy/start.jpg",
+  //     path: "C:/Users/111/Desktop/copy/start.jpg",
+  //     extname: ".jpg",
+  //     ext: "jpg",
+  //     outsrc:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
+  //     outpath:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
+  //     quality: "80",
+  //     rawDataSize: "5.57 KB",
+  //     nowDataSize: "2.28 KB",
+  //   },
+  //   {
+  //     state: "finish",
+  //     percentage: "-9.8%",
+  //     src: "file://C:/Users/111/Desktop/copy/start.jpg",
+  //     path: "C:/Users/111/Desktop/copy/start.jpg",
+  //     extname: ".jpg",
+  //     ext: "jpg",
+  //     outsrc:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
+  //     outpath:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
+  //     quality: "80",
+  //     rawDataSize: "5.57 KB",
+  //     nowDataSize: "2.28 KB",
+  //   },
+  //   {
+  //     state: "finish",
+  //     percentage: "-9.8%",
+  //     src: "file://C:/Users/111/Desktop/copy/start.jpg",
+  //     path: "C:/Users/111/Desktop/copy/start.jpg",
+  //     extname: ".jpg",
+  //     ext: "jpg",
+  //     outsrc:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg",
+  //     outpath:
+  //       "J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading",
+  //     quality: "80",
+  //     rawDataSize: "5.57 KB",
+  //     nowDataSize: "2.28 KB",
+  //   },
+  // ];
+  files:FILE[] = [];
+  constructor(
+    private electronService: ElectronService,
+    private store: Store<Array<FILE>>,
+    private cdr: ChangeDetectorRef,
+   
+  ) {
     this.files$ = store.pipe(select(selectFile));
-    this.files$.subscribe((value) => {
-      console.log("filesv", value);
-    });
+
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
 
   trackByItem(index: number, value: FILE): FILE["state"] {
     return value.state;
   }
 
   qualityChange(item: FILE, v: number): void {
-    item.state = "await";
-    item.quality = `${v}`;
     this.store.dispatch(
       UPDATE_STATE({
-        file: item,
+        files: {
+          ...item,
+          quality: `${v}`
+        }
       })
     );
   }
+
+  qualityAfterChange(item: FILE, v: number): void {
+    const newFileInfo:FILE = {
+      ...item,
+      state: "await",
+      quality: `${v}`
+    };
+
+    this.store.dispatch(
+      UPDATE_STATE({
+        files: newFileInfo
+      })
+    );
+
+    
+    this.electronService.file_update_quality(newFileInfo);
+  }
+
+  remove(key: string):void{
+    this.store.dispatch(
+      REMOVE_FILE({
+        keys: key
+      })
+    );
+  }
+  
 }
