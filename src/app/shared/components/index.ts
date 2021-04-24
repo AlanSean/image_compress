bff3837329ae561f827694442812aac9ab367854
huangqing <2316694914@qq.com>
2021-04-24 14:33:47 +0800

    styles: 优化结构

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,6 +1,6 @@
-export * from "./page-not-found/page-not-found.component";
-export * from "./icon/icon.component";
-export * from "./setting/setting.component";
-export * from "./setting/setting.module";
-export * from "./img-list/img-list.component";
-export * from "./img-list/img-list.module";
+import { ContextmenuComponent, ContextmenuDirective } from './contextmenu/contextmenu.component';
+import { IconComponent } from './icon/icon.component';
+import { ImgListComponent } from './img-list/img-list.component';
+import { SettingComponent } from './setting/setting.component';
+
+export default [IconComponent, SettingComponent, ImgListComponent, ContextmenuComponent, ContextmenuDirective];
