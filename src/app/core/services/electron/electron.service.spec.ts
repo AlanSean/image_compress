b0168487ee0c51b5b510950a59532e4be3c4f54e
huangqing <2316694914@qq.com>
2021-04-28 22:38:09 +0800

    test: 添加 TranslateModule

--- src/app/core/services/electron/electron.service.spec.ts
+++ src/app/core/services/electron/electron.service.spec.ts
@@ -3,14 +3,15 @@ import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 import { ElectronService } from './electron.service';
 import { NzMessageService, NzMessageModule } from 'ng-zorro-antd/message';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('ElectronService', () => {
   let store: MockStore;
   let message: NzMessageService;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideMockStore(), NzMessageService],
-      imports: [NzMessageModule]
+      providers: [provideMockStore(), NzMessageService, NzMessageService],
+      imports: [NzMessageModule, TranslateModule.forRoot()]
     });
     store = TestBed.inject(MockStore);
   });
