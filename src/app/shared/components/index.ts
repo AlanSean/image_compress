627bc2402ca235e63f4c746c21d5972938b1e040
huangqing <2316694914@qq.com>
2021-05-02 20:23:10 +0800

    feat: 新增图片预览

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -2,5 +2,14 @@ import { ContextmenuComponent, ContextmenuDirective } from './contextmenu/contex
 import { IconComponent } from './icon/icon.component';
 import { ImgListComponent } from './img-list/img-list.component';
 import { SettingComponent } from './setting/setting.component';
+import { CompareDirective, CompareComponent } from './compare/compare.component';
 
-export default [IconComponent, SettingComponent, ImgListComponent, ContextmenuComponent, ContextmenuDirective];
+export default [
+  IconComponent,
+  SettingComponent,
+  ImgListComponent,
+  ContextmenuComponent,
+  ContextmenuDirective,
+  CompareDirective,
+  CompareComponent
+];
