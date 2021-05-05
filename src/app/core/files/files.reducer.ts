da78a818bdfd0f53c7709c21694c923df551540c
huangqing <2316694914@qq.com>
2021-05-06 00:20:05 +0800

    fix: 多张图片状态更新问题

--- src/app/core/files/files.reducer.ts
+++ src/app/core/files/files.reducer.ts
@@ -20,14 +20,25 @@ export const fileReducer = createReducer(
       length: fileMap.getLen()
     };
   }),
+  //只接受添加并且状态从 await => finsh | error过渡
   on(FILE_ADD, (_, { files }) => {
     //如果是数组
     if (Array.isArray(files)) {
       for (const file of files) {
+        //searchImg 也许会比 压缩完成时要慢
+        //因为图片压缩是批量压缩,而添加file是同步的
+        if (fileMap.get(file.MD5KEY)?.state !== 'await' && file.state == 'await') {
+          continue;
+        }
         fileMap.put(file.MD5KEY, file);
       }
     } else {
-      fileMap.put(files.MD5KEY, files);
+      //...
+      //!fileMap.has(files.MD5KEY) 不存在 直接添加
+      //如果存在files.state !== 'await'//
+      if (!fileMap.has(files.MD5KEY) || files.state !== 'await') {
+        fileMap.put(files.MD5KEY, files);
+      }
     }
     return {
       fileArr: fileMap.getArrayVal(),
