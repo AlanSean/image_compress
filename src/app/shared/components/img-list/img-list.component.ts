771f39c624d0563f5f5f814b98c90da9fc08092e
huangqing <2316694914@qq.com>
2021-05-07 20:13:41 +0800

    feat: New REMOVE_FILE method

--- src/app/shared/components/img-list/img-list.component.ts
+++ src/app/shared/components/img-list/img-list.component.ts
@@ -4,31 +4,34 @@ import { FILE } from '@common/constants';
 import { selectFile, UPDATE_STATE, REMOVE_FILE } from '@app/core/core.module';
 import { ElectronService } from '@app/core/services';
 import { NzModalService } from 'ng-zorro-antd/modal';
-
+import { throttleTime } from 'rxjs/operators';
 import { data } from './data';
 
 @Component({
   selector: 'app-img-list',
   templateUrl: './img-list.component.html',
   styleUrls: ['./img-list.component.less']
-  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ImgListComponent implements OnInit {
   isOpen = false;
-  files$ = this.store.pipe(select(selectFile));
-  files = data;
+  files$ = this.store.pipe(throttleTime(100), select(selectFile));
+  files: readonly FILE[] = [];
+
+  //删除
+  remove = this.electronService.remove;
 
   @ViewChild('contextmenuEl') contextmenuEl!: ElementRef;
   constructor(
     private electronService: ElectronService,
     private store: Store<Array<FILE>>,
-    // private cdr: ChangeDetectorRef,
+    private cdr: ChangeDetectorRef,
     private modal: NzModalService
   ) {}
   ngOnInit() {
     this.files$.subscribe(v => {
-      // this.cdr.detectChanges();
-      console.log(v.length);
+      this.files = v;
+      console.log(this.files);
+      this.cdr.detectChanges();
     });
   }
   trackByItem(index: number, value: FILE) {
@@ -36,39 +39,16 @@ export class ImgListComponent implements OnInit {
   }
   //拖动滑块
   qualityChange(item: FILE, v: number) {
-    this.store.dispatch(
-      UPDATE_STATE({
-        files: {
-          ...item,
-          quality: `${v}`
-        }
-      })
-    );
+    item.quality = `${v}`;
+    this.electronService.update('_', item);
   }
 
   //滑块设置完质量后
   qualityAfterChange(item: FILE, v: number | number[]) {
-    const newFileInfo: FILE = {
-      ...item,
-      state: 'await',
-      quality: `${v}`
-    };
-
-    this.store.dispatch(
-      UPDATE_STATE({
-        files: newFileInfo
-      })
-    );
-    this.electronService.file_update_quality(newFileInfo);
-  }
-
-  //删除
-  remove(key: string): void {
-    this.store.dispatch(
-      REMOVE_FILE({
-        keys: key
-      })
-    );
+    item.state = 'await';
+    item.quality = `${v}`;
+    this.electronService.update('_', item);
+    this.electronService.file_update_quality(item);
   }
 
   //信息框
@@ -90,11 +70,7 @@ export class ImgListComponent implements OnInit {
         this.electronService.saveAs(item);
         break;
       case 'remove':
-        this.store.dispatch(
-          REMOVE_FILE({
-            keys: item.MD5KEY
-          })
-        );
+        this.remove(item.MD5KEY);
         break;
     }
   }
