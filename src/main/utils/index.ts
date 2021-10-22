6e99f8cf040aa70a39d581fd94ceb9a547565be8
huangqing <2316694914@qq.com>
2021-10-22 14:20:02 +0800

    feat: debug log

--- src/main/utils/index.ts
+++ src/main/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './locales';
 export * from './utils';
+export * from './log';
---
+++ src/main/utils/log.ts
@@ -0,0 +1,11 @@
+import { isServe } from './utils';
+
+let electronLog: any = null;
+
+export function log(...msg: any[]) {
+  if (isServe && electronLog == null) {
+    electronLog = require('electron-log');
+    electronLog.transports.console.level = 'silly';
+  }
+  isServe && electronLog && electronLog.log(...msg);
+}
