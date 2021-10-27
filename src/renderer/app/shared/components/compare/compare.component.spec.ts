565a12e4b62b52ab5726901533c527161250c6bf
huangqing <2316694914@qq.com>
2021-10-27 10:39:39 +0800

    test: perfect appCompareData

--- src/renderer/app/shared/components/compare/compare.component.spec.ts
+++ src/renderer/app/shared/components/compare/compare.component.spec.ts
@@ -8,7 +8,24 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
     <div appCompare [appCompareData]="appCompareData"></div>
   `
 })
-class TestComponent {}
+class TestComponent {
+  appCompareData = {
+    state: 'await',
+    MD5KEY: 'q23',
+    outdir: '',
+    name: '',
+    percentage: '-9.8%',
+    src: 'xxxx.jpg',
+    path: 'xxxx.jpg',
+    extname: '.jpg',
+    ext: 'jpg',
+    outsrc: 'xxxx.jpg',
+    outpath: 'xxxx',
+    quality: '80',
+    rawDataSize: '5.57 KB',
+    nowDataSize: '2.28 KB'
+  };
+}
 
 describe('appCompare', () => {
   let component: TestComponent;
