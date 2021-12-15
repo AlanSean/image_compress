f72a7f228f4cc32ea1cb4db8b9a33eeaa06f19bd
AlanSean <2316694914@qq.com>
2021-12-15 19:43:18 +0800

    test: fix test bug

--- src/renderer/app/home/home.component.spec.ts
+++ src/renderer/app/home/home.component.spec.ts
@@ -1,49 +1,27 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterTestingModule } from '@angular/router/testing';
-
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
-import { selectFile, getFilesLength, CoreModule } from '../core/core.module';
-
 import { NzProgressModule } from 'ng-zorro-antd/progress';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
-  let store: MockStore;
-  const initialState = { length: 0, filesArr: [] };
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
         declarations: [HomeComponent],
-        imports: [TranslateModule.forRoot(), RouterTestingModule, SharedModule, CoreModule, NzProgressModule, NzInputModule, NzMenuModule],
-        providers: [provideMockStore({ initialState })]
+        imports: [
+          TranslateModule.forRoot(),
+          RouterTestingModule,
+          SharedModule,
+          NzProgressModule,
+          NzInputModule,
+          NzMenuModule,
+        ],
       }).compileComponents();
-      store = TestBed.inject(MockStore);
-      store.overrideSelector(getFilesLength, 0);
-      store.overrideSelector(selectFile, [
-        {
-          MD5KEY: '',
-          name: '',
-          outdir: '',
-          state: 'finish',
-          percentage: '-9.8%',
-          src: 'file://C:/Users/111/Desktop/copy/start.jpg',
-          path: 'C:/Users/111/Desktop/copy/start.jpg',
-          extname: '.jpg',
-          ext: 'jpg',
-          outsrc:
-            'J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading\\start.jpg',
-          outpath:
-            'J:\\QQ几率\\2316694914\\FileRecv\\剑灵小助手1.7.7(密码为jlxzs)\\剑灵工具箱 V2.91\\剑灵工具箱 V2.91\\Resources\\HaoZip\\Loading',
-          quality: '80',
-          rawDataSize: '5.57 KB',
-          nowDataSize: '--'
-        }
-      ]);
     })
   );
 
